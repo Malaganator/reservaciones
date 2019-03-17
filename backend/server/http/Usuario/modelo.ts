e49966733cc3d8bfe55e47fa128bc25bf4e110f8
Luis Malaga <luisgateshouse@gmail.com>
2019-03-16 18:07:40 -0600

    modificando usuarios

--- backend/server/http/Usuario/modelo.ts
+++ backend/server/http/Usuario/modelo.ts
@@ -1,47 +1,12 @@
-
+const data = require('../../conf/usuarios.data.json');
+import fs from 'fs'
+import path from 'path';
 export class Usuario{
     private _id: number;
     private _nombre: string;
     private _correo: string; 
     private _contrasena: string;
-    private static _usuarios: Usuario[] = [
-        new Usuario({
-            id: 1,
-            nombre: 'Ulises Gonzalez',
-            correo: 'ulises@obisey.com',
-            contrasena: '1234'
-        }),
-        new Usuario({
-            id: 2,
-            nombre: 'Cesar Cordoba',
-            correo: 'cesar@obisey.com',
-            contrasena: '1234'
-        }),
-        new Usuario({
-            id: 3,
-            nombre: 'Ramon',
-            correo: 'ramon@obisey.com',
-            contrasena: '1234'
-        }),
-        new Usuario({
-            id: 4,
-            nombre: 'Josue Yepez',
-            correo: 'josue@obisey.com',
-            contrasena: '1234'
-        }),
-        new Usuario({
-            id: 5,
-            nombre: 'Fernando Jerezano',
-            correo: 'jerezano@obisey.com',
-            contrasena: '1234'
-        }),
-        new Usuario({
-            id: 6,
-            nombre: 'Luis Malaga',
-            correo: 'malaga@obisey.com',
-            contrasena: '1234'
-        }),                                                            
-    ];
+    private static _usuarios: Usuario[] = data.Usuarios.map(n => new Usuario(n));
 
     constructor(arg){
         this._id = arg.id;
@@ -52,16 +17,19 @@ export class Usuario{
 
     public static crear(arg){
         return new Promise<Usuario>((resolve, reject) => {
-            resolve(new Usuario(arg));
+            this._usuarios.push(new Usuario(arg))
+            let json = {"Usuarios": this._usuarios.map(n => n.toJson())}
+            this.sobreEscribir(json)
+                .then(response=> response ? resolve(new Usuario(arg)) : reject(false))
         })
     }
 
     public static buscarPorId(id: number){
         return new Promise<Usuario>((resolve, reject) => {
             let usuario = this._usuarios.find(usuario => usuario._id == id);
-
             if(usuario){
-                resolve(usuario);
+                resolve(usuario)
+
             }else{
                 reject('No se encontró el usuario we');
             }
@@ -77,10 +45,11 @@ export class Usuario{
     public static eliminar(id: number){
         return new Promise<boolean>((resolve, reject) => {
             let usuario = this._usuarios.find(usuario => usuario._id == id);
-
             if(usuario){
-                console.log('Eliminando...')
-                resolve(true);
+                this._usuarios.splice(this._usuarios.indexOf(usuario), 1)
+                let json = {"Usuarios": this._usuarios.map(n=> n.toJson())}
+                this.sobreEscribir(json)
+                    .then(response=> response ? resolve(true) : reject(false))
             }else{
                 resolve(false);
             }
@@ -90,11 +59,15 @@ export class Usuario{
     public static editar(arg){
         return new Promise<boolean>((resolve, reject) => {
             let usuario = this._usuarios.find(usuario => usuario._id == arg.id);
-
             if(usuario){
+                let index =this._usuarios.indexOf(usuario);
                 usuario.nombre = arg.nombre ? arg.nombre : usuario.nombre;
                 usuario.correo = arg.correo ? arg.correo : usuario.correo;
                 usuario.contrasena = arg.contrasena ? arg.contrasena : usuario.contrasena;
+                this._usuarios[index] = usuario
+                let json = {"Usuarios": this._usuarios.map(n => n.toJson())}
+                this.sobreEscribir(json)
+                    .then(response=> response ? resolve(true) : reject(false))
                 resolve(true)
             }else{
                 resolve(false);
@@ -132,5 +105,39 @@ export class Usuario{
 
     set contrasena(value: string){
         this._contrasena = value;
-    }    
+    } 
+
+    public toJson(){
+        return {
+            "id": this._id,
+            "nombre": this._nombre,
+            "correo": this._correo,
+            "contrasena":this._contrasena
+        }
+    }
+    
+    private static sobreEscribir(json){
+        return new Promise<boolean>((resolve, reject) => {
+            fs.exists(
+                path.join(__dirname, '../../conf/usuarios.data.json'),
+                (exist) => {
+                    if(exist){
+                        console.log('si existe')
+                        fs.writeFile(
+                            path.join(__dirname, '../../conf/usuarios.data.json'),
+                            JSON.stringify(json, null, 4),
+                            (err) => {
+                                console.log(err)
+                                if(err)
+                                    reject(err)
+                                resolve(true)
+                            }
+                        )
+                    }else{
+                        console.log('no existe')
+                        reject(false);
+                    }
+            })
+        })
+    }
 }
